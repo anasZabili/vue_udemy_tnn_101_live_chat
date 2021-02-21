718d5fed151a37c77fda541763fe349991c44ca8
anas zabili <anaszab78@gmail.com>
2021-02-21 19:19:21 +0100

    lesson 103

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import { isNavigationFailure } from "vue-router";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8LzarLqGpTHuKb_F-7-JxEUefG9se80U",
+  authDomain: "udemy-vue-tnn-blog.firebaseapp.com",
+  projectId: "udemy-vue-tnn-blog",
+  storageBucket: "udemy-vue-tnn-blog.appspot.com",
+  messagingSenderId: "285478383757",
+  appId: "1:285478383757:web:e59244b8a9cdadcdd932c4",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, timestamp };
